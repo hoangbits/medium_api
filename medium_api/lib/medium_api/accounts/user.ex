c68562ea73789b2ebcdc3d8ec04cc42c9b11de7e
hoang <giahoangth@gmail.com>
2021-03-11 21:45:17 +0700

    feat: add pw hash func

--- medium_api/lib/medium_api/accounts/user.ex
+++ medium_api/lib/medium_api/accounts/user.ex
@@ -34,6 +34,11 @@ defmodule MediumApi.Accounts.User do
     |> hash_password
   end
 
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
+
   defp hash_password(changeset) do
     changeset
   end
--- medium_api/mix.exs
+++ medium_api/mix.exs
@@ -42,7 +42,9 @@ defmodule MediumApi.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 5.3"},
+      {:argon2_elixir, "~> 2.4"}
     ]
   end
 
