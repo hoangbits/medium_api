4335f27dc5ed20c24ecf7fb13cbfa972ad61ff2e
hoang <giahoangth@gmail.com>
2021-03-13 21:30:11 +0700

    feat: add absinthe packages

--- medium_api/mix.exs
+++ medium_api/mix.exs
@@ -43,7 +43,10 @@ defmodule MediumApi.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:argon2_elixir, "~> 2.4"}
+      {:argon2_elixir, "~> 2.4"},
+      {:absinthe, "~> 1.6"},
+      {:absinthe_phoenix, "~> 2.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
